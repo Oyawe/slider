3e16e3e11308527a83298161dcf27bf7758a5afa
Oyawe <oyawegoodness@gmail.com>
2023-10-02 04:41:53 -0700

    cleaned the code

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
-import { shortList, list, longList } from "./data";
+import { list } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 
 const SlickCarousel = () => {
